99ce5bf307cd904d6c1994cefb64fe17dfa60368
Timur <timur@fralik.ru>
2020-06-21 02:01:35 +0300

    Install Laravel

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+//require('./bootstrap');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routes from './routes';
+
+Vue.use(VueRouter);
+
+//window.Vue = require('vue');
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+
+    router: new VueRouter(routes)
+});
