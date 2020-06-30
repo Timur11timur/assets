6f456624194e95ffa26927414f340f77b0338e9e
Timur <timur@fralik.ru>
2020-07-01 01:14:13 +0300

    After 6 video

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -1,35 +1,70 @@
 <template>
     <div>
-        <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-8">Full Logo</h1>
+        <h1 class="font-normal text-3xl text-grey-darkest leading-none mb-8">
+            Full Logo
+        </h1>
 
-        <div class="mb-10">
-            <picture style="height: 382px;" class="block shadow rounded-lg flex items-center justify-center mb-6">
+        <!-- Original Logo -->
+        <div class="mb-12">
+            <picture
+                style="height: 382px;"
+                class="block shadow rounded-lg flex items-center justify-center mb-6"
+            >
                 <img src="/images/logo.svg" alt="logo" />
             </picture>
+
             <div class="flex justify-between items-center">
-                <p>
-                    Original Logo on <strong>white</strong> background
-                </p>
-                <a href="/images/logo.svg" class="border border-grey-light px-8 py-2 rounded-full text-black font-bold text-xs">logo.svg</a>
+                <p>Original logo on <strong>white</strong> background</p>
+
+                <a href="/images/logo.svg" class="button" download>logo.svg</a>
             </div>
         </div>
 
-        <div class="mb-10">
-            <picture style="height: 382px;" class="block shadow rounded-lg flex items-center justify-center mb-6 bg-blue">
+        <!-- Negative Logo With Accent -->
+        <div class="mb-12">
+            <picture
+                style="height: 382px;"
+                class="block shadow rounded-lg flex items-center justify-center mb-6 bg-blue"
+            >
                 <img src="/images/negative-logo.svg" alt="logo" />
             </picture>
+
             <div class="flex justify-between items-center">
                 <p>
-                    Negative logo on <strong>blue</strong> background
+                    Negative logo with accent on
+                    <strong>dark</strong> background
                 </p>
-                <a href="/images/negative-logo.svg" class="border border-grey-light px-8 py-2 rounded-full text-black font-bold text-xs">negative-logo.svg</a>
+
+                <a href="/images/negative-logo.svg" class="button" download>
+                    negative-logo.svg
+                </a>
+            </div>
+        </div>
+
+        <!-- Pure Negative Logo -->
+        <div>
+            <picture
+                style="height: 382px;"
+                class="block shadow rounded-lg flex items-center justify-center mb-6 bg-black"
+            >
+                <img src="/images/pure-negative-logo.svg" alt="logo" />
+            </picture>
+
+            <div class="flex justify-between items-center">
+                <p>Pure negative logo on <strong>dark</strong> background</p>
+
+                <a
+                    href="/images/pure-negative-logo.svg"
+                    class="button"
+                    download
+                >
+                    pure-negative-logo.svg
+                </a>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-
-    };
+export default {};
 </script>
